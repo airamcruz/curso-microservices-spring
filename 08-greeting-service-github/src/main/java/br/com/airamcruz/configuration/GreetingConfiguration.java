61e31b57a8f5c820ada526f68c7018971c38f794
Leonardo Airam Muniz Cruz <aiiramcruz@gmail.com>
2023-07-12 23:20:20 -0300

    integrando greeting service com github

---
+++ 08-greeting-service-github/src/main/java/br/com/airamcruz/configuration/GreetingConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.airamcruz.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(value = "greeting-service")
+public class GreetingConfiguration {
+	
+	private String greeting;
+	private String defaultValue;
+	
+	
+	public GreetingConfiguration() {
+	}
+
+	public GreetingConfiguration(String greeting, String defaultValue) {
+		this.greeting = greeting;
+		this.defaultValue = defaultValue;
+	}
+
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+	
+}
